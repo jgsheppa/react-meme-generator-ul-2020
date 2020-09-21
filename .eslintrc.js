fbd7f5ab528e9ce743a6c3a0dfffa0faa0329fd6
James Sheppard <jamessheppard@Verenas-MBP.public.ph.local>
2020-09-21 16:26:08 +0200

    Added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  extends: 'react-app',
+  plugins: ['cypress'],
+  env: {
+    'cypress/globals': true,
+  },
+  rules: {
+    // Error out on imports that don't match
+    // the underlying file system
+    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-unresolved.md
+    'import/no-unresolved': 'error',
+    // Warn about constant conditions
+    // https://eslint.org/docs/rules/no-constant-condition
+    'no-constant-condition': 'warn',
+    // Warn about variable shadowing
+    // https://eslint.org/docs/rules/no-shadow
+    'no-shadow': 'warn',
+    // Remove `href` warnings on anchor tags for Next.js
+    // Issue in Next.js: https://github.com/zeit/next.js/issues/5533
+    // Fix: https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/issues/402#issuecomment-368305051
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: ['Link'],
+        specialLink: ['hrefLeft', 'hrefRight'],
+        aspects: ['invalidHref', 'preferButton'],
+      },
+    ],
+    // Disable rule because the new JSX transform in React 17,
+    // Next.js and Gatsby no longer requires the import.
+    //
+    // https://github.com/yannickcr/eslint-plugin-react/issues/2440#issuecomment-683433266
+    'react/react-in-jsx-scope': 'off',
+  },
+};
